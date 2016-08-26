69790e39f202c502ef8676dbc2ff51509c515b95
Alvin Ng <alvindng@gmail.com>
2016-08-26 15:52:12 -0700

    add a custom helper to signify when a question has 3 or more answers:

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') >= 3) {
+    return Ember.String.htmlSafe('<h6> <span class="glyphicon glyphicon-fire"></span> Popular Question!</h6>');
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);
