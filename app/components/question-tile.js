35c15a8a229e1cbe6ea6f7535224e1a2c087fe19
Alvin Ng <alvindng@gmail.com>
2016-08-26 17:32:20 -0700

    add a button to add a service to keep track of the users' favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
   answerCount: Ember.computed('question.answers', function() {
     return this.get('question.answers.length');
-  })
+  }),
+  actions: {
+    addToFavorites(item) {
+      this.get('favoriteQuestions').add(item);
+    },
+  },
 });
--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function questionPopularity(params) {
   var question = params[0];
 
-  if(question.get('answers').get('length') >= 3) {
+  if(question.get('answers').get('length') >= 5) {
     return Ember.String.htmlSafe('<h6> <span class="glyphicon glyphicon-fire"></span> Popular Question!</h6>');
   }
 }
